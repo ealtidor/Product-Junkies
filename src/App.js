3b2cc68438fdca68ecd006fa84304e76620da8ab
ealtidor <emanuella.altidor@gmail.com>
2021-05-10 12:26:48 -0400

    Updated Search Bar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import Nav from "./components/Nav"
 import Form from "./components/Form"
 import { Route} from "react-router-dom"
 import Home from "./components/Home"
-import { useEffect, useState } from "react";
 import axios from "axios";
 import { baseURL, config } from "./services";
 import './App.css';
 import Favorites from "./components/Favorites";
 import Filter from "./components/Filter"
+import { useState, useEffect } from "react";
 
 
 // Get Request
@@ -15,27 +15,27 @@ function App() {
   const [posts, setPosts] = useState([]);
   const [toggleFetch, setToggleFetch] = useState(false);
 
+
   useEffect(() => {
     const fetchPosts = async () => {
       const resp = await axios.get(baseURL, config);
       setPosts(resp.data.records);
-    }
+    };
     fetchPosts();
-  }, [toggleFetch])
-
+  }, [toggleFetch]);
 
   return (
     <div className="App">
-      <Nav />
+      <Nav posts={posts}/>
       <hr />
       <Route path="/favorites">
         <Favorites/>
       </Route>
       <Route exact path="/">
-      <Home posts={posts} setToggleFetch={setToggleFetch}/>
+      <Home posts={posts} setToggleFetch={setToggleFetch} />
       </Route>
       <Route path="/new">
-        <Form  setToggleFetch={setToggleFetch}/>
+        <Form setToggleFetch={setToggleFetch}  />
       </Route>
       <Route path="/filter" component={Filter}/>
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,42 @@
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faPlus, faHeart} from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faHeart } from '@fortawesome/free-solid-svg-icons'
+import {useState} from "react"
 
-function Nav() {
-  
+function Nav(props) {
+  // some state to store the seatrch term
+  const [findPost, setFindPost] = useState("")
+  const matchPost = props.posts.filter((post) =>
+  post.fields.category.toLowerCase().includes(findPost.toLocaleLowerCase()))
+console.log(matchPost)
+  // filter through posts from props to find any .fields.category
 
   return (
     <nav className="navbar">
-      <Link className="header" to="/">Product Junkies</Link>
+      <Link className="header" to="/">Product JunkEz</Link>
+      
+      <div classname="typehead-container">
       <input
         onClick="enter"
         className="search"
-        type="text"
+          type="text"
+          value={findPost}
+          onChange= {(e) => setFindPost(e.target.value)}
         name="searchProduct"
         id="search-product"
-        placeholder="SEARCH JUNK DRAW" />
+          placeholder="SEARCH JUNK DRAWER" />
+        
+        {findPost && (
+          <div className="results-container">
+            {matchPost.map((post) => (
+          <p>{post.fields.category}</p>
+            ))}
+          </div>
+        )
+        }
+      </div>
+      
+     
       <Link className="plus-sign" to="/favorites"><FontAwesomeIcon icon={ faHeart} size="2x"/></Link>
       <Link className="plus-sign" to="/new"><FontAwesomeIcon icon={ faPlus} size="2x"/></Link>
     </nav>
