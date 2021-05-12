4e6ab277f4a0f96f87f1f91dd2143e0b88201c22
ealtidor <emanuella.altidor@gmail.com>
2021-05-11 21:34:25 -0400

    tablet size responsiveness

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,31 +1,32 @@
-import { Link } from "react-router-dom"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus, faHeart } from '@fortawesome/free-solid-svg-icons'
-
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faHeart } from "@fortawesome/free-solid-svg-icons";
 
 function Nav(props) {
-  
-
   return (
     <nav className="navbar">
-      <Link className="header" to="/">Product JunkEz</Link>
-      
-      <div classname="typehead-container">
+      <Link className="header" to="/">
+        Product JunkEz
+      </Link>
+
       <input
         className="search"
-          type="text"
-          value={props.findPost}
-          onChange= {(e) => props.setFindPost(e.target.value)}
+        type="text"
+        value={props.findPost}
+        onChange={(e) => props.setFindPost(e.target.value)}
         name="searchProduct"
         id="search-product"
-          placeholder="SEARCH JUNK DRAWER" />
-      </div>
-      
-     
-      <Link className="plus-sign" to="/favorites"><FontAwesomeIcon icon={ faHeart} size="2x"/></Link>
-      <Link className="plus-sign" to="/new"><FontAwesomeIcon icon={ faPlus} size="2x"/></Link>
+        placeholder="SEARCH JUNK DRAWER"
+      />
+
+      <Link className="plus-sign" to="/favorites">
+        <FontAwesomeIcon icon={faHeart} size="2x" />
+      </Link>
+      <Link className="plus-sign" to="/new">
+        <FontAwesomeIcon icon={faPlus} size="2x" />
+      </Link>
     </nav>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
